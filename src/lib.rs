38c1e11c039163cf08a6a6ecc9bf5b02922e3957
Raas Ahsan <raas.ahsan@gmail.com>
2021-12-04 13:39:44 -0600

    Set up wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn run() -> u8 {
+    8
 }
