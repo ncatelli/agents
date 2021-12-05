d4319f873ba6b643470908ba946fea799c4d4a96
Nate Catelli <ncatelli@packetfire.org>
2021-12-05 17:29:24 +0000

    docstrings on Command

--- src/ast.rs
+++ src/ast.rs
@@ -30,31 +30,54 @@ impl Agent {
     }
 }
 
+/// All valid Command variants that an Agent can perform.
 #[derive(Debug, Clone, PartialEq)]
 pub enum Command {
+    /// Sets a variable identified by the string to the primitive evaluated to by
+    /// the associated Expression.
     SetVariable(String, Expression),
+    /// Defines the direction an agent should face.
     Face(Direction),
+    /// Turns by a number of rotations where a positive number represents a
+    /// clockwise rotation and a negavite represents a counter-clockwise
+    /// rotation.
     Turn(i32),
+    /// Move specifies the steps that an agent will move in the direction it is
+    /// facing.
     Move(u32),
+    /// Goto jumps to the enclosed offset in an agents command list.
     Goto(u32),
+    /// Like Goto, JumpTrue jumps to the enclosed offset if the passed conditional
+    /// expression evaluates to true.
     JumpTrue(u32, Expression),
 }
 
+/// Sets a variable identified by the string to the primitive evaluated to by
+/// the associated Expression on an associated agent..
 #[derive(Debug, Clone, PartialEq)]
 pub struct SetVariableCmd(pub String, pub Expression);
 
+/// Defines the direction an agent should face.
 #[derive(Debug, Clone, PartialEq)]
 pub struct FaceCmd(pub Direction);
 
+/// Turns by a number of rotations where a positive number represents a
+/// clockwise rotation and a negavite represents a counter-clockwise
+/// rotation.
 #[derive(Debug, Clone, PartialEq)]
 pub struct TurnCmd(pub i32);
 
+/// Move specifies the steps that an agent will move in the direction it is
+/// facing.
 #[derive(Debug, Clone, PartialEq)]
 pub struct MoveCmd(pub u32);
 
+/// Goto jumps to the enclosed offset in an agents command list.
 #[derive(Debug, Clone, PartialEq)]
 pub struct GotoCmd(pub u32);
 
+/// Like Goto, JumpTrue jumps to the enclosed offset if the passed conditional
+/// expression evaluates to true.
 #[derive(Debug, Clone, PartialEq)]
 pub struct JumpTrueCmd(pub u32, pub Expression);
 
