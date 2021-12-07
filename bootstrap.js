118ca01a1b3453bd63f33f5e95fd8cd543cc0260
ncatelli <ncatelli@users.noreply.github.com>
2021-12-07 21:11:13 +0000

    deploy: c5c43bb9a93ee6208e000950e92b8ededeef5b7f

--- bootstrap.js
+++ bootstrap.js
@@ -159,7 +159,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/agents_bg.wasm":"18ffb67540c7d9e248ea"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/agents_bg.wasm":"52d18129efbc92d34222"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
